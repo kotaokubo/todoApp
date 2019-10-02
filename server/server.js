1669366c065c94bb27a047119a095d3f6bf6d80f
kotaokubo <kutpremium@gmail.com>
2019-10-02 21:16:41 +0900

    CORSのエラーが出ます

--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.json({
     message: "成功！"
   });
@@ -22,13 +22,14 @@ router.get("/", function(req, res) {
 
 router
   .route("/todos")
-  .post(function(req, res) {
+  .post(function (req, res) {
     const todo = new Todo();
 
     todo.title = req.body.title;
     todo.desc = req.body.desc;
+    todo.isDone = req.body.isDone;
 
-    todo.save(function(err) {
+    todo.save(function (err) {
       if (err) {
         res.send(err);
       }
@@ -37,8 +38,8 @@ router
       });
     });
   })
-  .get(function(req, res) {
-    Todo.find(function(err, todos) {
+  .get(function (req, res) {
+    Todo.find(function (err, todos) {
       if (err) {
         res.send(err);
       }
@@ -47,20 +48,20 @@ router
   });
 router
   .route("/todos/:todo_id")
-  .get(function(req, res) {
-    Todo.findById(req.params.todo_id, function(err, todo) {
+  .get(function (req, res) {
+    Todo.findById(req.params.todo_id, function (err, todo) {
       if (err) {
         res.send(err);
       }
       res.json(todo);
     });
   })
-  .delete(function(req, res) {
+  .delete(function (req, res) {
     Todo.remove(
       {
         _id: req.params.todo_id
       },
-      function(err, todo) {
+      function (err, todo) {
         if (err) {
           res.send(err);
         }
@@ -71,6 +72,11 @@ router
     );
   });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use("/api", router);
 
 app.listen(port, () => {
